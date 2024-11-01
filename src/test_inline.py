470771c3a5b614bd4433c6e44f9ac393bdb56006
PlumStone70486 <tsbg95@gmail.com>
2024-11-01 14:08:39 +0100

    added text to textnodes function and test

--- src/test_inline.py
+++ src/test_inline.py
@@ -136,5 +136,22 @@ class TestInlineMarkdown(unittest.TestCase):
         ]
         self.assertEqual(new_nodes, expected_nodes)
 
+    def test_text_to_nodes(self):
+        old_nodes = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(old_nodes)
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
 if __name__ == "__main__":
     unittest.main()
