e201178370f7a09968d76047569a6092386e17d1
PlumStone70486 <tsbg95@gmail.com>
2024-11-03 11:18:54 +0100

    added generate page function and corrected blocks to html functions

--- src/main.py
+++ src/main.py
@@ -26,6 +26,28 @@ def copy_files(src, dest):
             os.mkdir(new_dest)
             copy_files(new_src, new_dest)
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(from_path, "r") as file:
+        markdown_file = file.read()
+        html_title = extract_title(markdown_file)
+        html_node = markdown_to_html_node(markdown_file)
+        html_text = html_node.to_html()
+
+    with open(template_path, "r") as file:
+        template_file = file.read()
+        template_file = template_file.replace("{{ Title }}", html_title)
+        template_file = template_file.replace("{{ Content }}", html_text)
+
+    with open(dest_path, "w") as file:
+        file.write(template_file)
+    
+
 def main():
 
     node = HTMLNode("a", "Click me!", None, {"href": "https://boot.dev"})
@@ -35,5 +57,8 @@ def main():
     print(node2)
 
     copy_static()
+    generate_page("/home/tsbg/workspace/github.com/PlumStone70486/static_site_generator/content/index.md",
+                  "/home/tsbg/workspace/github.com/PlumStone70486/static_site_generator/template.html",
+                  "/home/tsbg/workspace/github.com/PlumStone70486/static_site_generator/public/index.html")
 
 main()
